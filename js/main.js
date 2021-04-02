f09deff3c018a5b207974af672c68c0d2ba60714
KovacAca <kovacaleksandar1@gmail.com>
2021-04-03 01:16:25 +0200

    01.04.video

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-// $('h1').hide(3000);
+// typed biblioteka
+var typed = new Typed('.element', {
+  // Waits 1000ms after typing "First"
+  strings: ['Kodiranje uzivo sredom', '30 dana kodiranja', 'JS radionica', 'Custom WP radionica'],
+  loop: true,
+  typeSpeed: 100,
+  smartBackspace: true,
+  backSpeed: 100,
+  loopCount: Infinity
+});
+
+//navigacija
+$(window).on('scroll', function() {
+  var scroll = $(window).scrollTop();
+  if(scroll >= 100) {
+    $('nav').addClass('stickyAdd');
+  } else {
+    $('nav').removeClass('stickyAdd');
+  }
+});
